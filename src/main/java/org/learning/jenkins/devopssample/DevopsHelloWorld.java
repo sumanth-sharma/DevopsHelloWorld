d0235a72789678747eba4eeba1fd7d0ba748b25f
Sumanth Sharma <sumanth.sharma21@gmail.com>
2021-05-10 12:42:58 +0530

    Added sample code

---
+++ src/main/java/org/learning/jenkins/devopssample/DevopsHelloWorld.java
@@ -0,0 +1,8 @@
+package org.learning.jenkins.devopssample;
+
+public class DevopsHelloWorld {
+    public static void main(String args[])  //static method
+    {
+        System.out.println("Hello world");
+    }
+}
